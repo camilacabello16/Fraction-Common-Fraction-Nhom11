94bb934cc65f79b867cc2e7b6b49f8d0847eb578
camilacabello16 <55092388+camilacabello16@users.noreply.github.com>
2020-04-29 22:01:04 +0700

    Update main.js

--- DragonLearn_html/JS/main.js
+++ DragonLearn_html/JS/main.js
@@ -16,17 +16,37 @@ $(document).ready(function(){
         right_answer_audio.play();
     }),
     //lesson 2
-    $('#wrong_answer_3').click(function(event){
-        $('#wrong_answer_3').addClass('test');
+    $('#wrong-answer-2').click(function(event){
         wrong_answer_audio.play();
-        setTimeout(function(){$('#wrong_answer_3').removeClass('test');},1300);
     }),
-    $('#wrong_answer_4').click(function(event){
-        $('#wrong_answer_4').addClass('test');
+    $('#wrong-answer-1').click(function(event){
         wrong_answer_audio.play();
-        setTimeout(function(){$('#wrong_answer_4').removeClass('test');},1300);
     }),
-    $('#right_answer_2').click(function(event){
+    $('#right-ans').click(function(event){
+        right_answer_audio.play();
+    }),
+    $('#wrong-screen-2').click(function(event){
+        wrong_answer_audio.play();
+    }),
+    $('#right-screen-2').click(function(event){
+        right_answer_audio.play();
+    }),
+    $('#wrong-screen-3').click(function(event){
+        wrong_answer_audio.play();
+    }),
+    $('#right-screen-3').click(function(event){
+        right_answer_audio.play();
+    }),
+    $('#wrong-screen-4').click(function(event){
+        wrong_answer_audio.play();
+    }),
+    $('#right-screen-4').click(function(event){
+        right_answer_audio.play();
+    }),
+    $('#wrong-screen-5').click(function(event){
+        wrong_answer_audio.play();
+    }),
+    $('#right-screen-5').click(function(event){
         right_answer_audio.play();
     })
     //lesson 3
